55fb13e16c0e61f144e18417746113774b8b880b
FernDev <75585604+FernandoEscuderoDev@users.noreply.github.com>
2023-02-14 04:41:03 +0100

    b

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -93,7 +93,7 @@ function Pomodoro(props) {
       justifyContent={"center"}
       alignItems={"center"}
       h="100vh"
-      gap={8}
+      gap={5}
       fontWeight={500}
     >
       <CircularProgress
